1ba4d686aafb064df5b8057fd63870f2cc68cfa3
Tobshub <tobanigeorge@gmail.com>
2023-10-11 19:21:02 +0100

    fix: log without pino-pretty

--- src/server/logger.ts
+++ src/server/logger.ts
@@ -1,7 +1,3 @@
 import { pino } from "pino";
-import { env } from "@/env.mjs";
 
-export const logger = pino({
-  transport:
-    env.NODE_ENV === "development" ? { target: "pino-pretty" } : undefined,
-});
+export const logger = pino({});
