146a64a12f5783a85560010183248622a4d8dd4c
Tobshub <tobanigeorge@gmail.com>
2023-10-25 13:40:47 +0100

    feat: default dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 
 import { api } from "@/utils/api";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -9,8 +10,14 @@ import "@fontsource/roboto/700.css";
 
 import "@/styles/globals.css";
 
+const theme = createTheme({ palette: { mode: "dark" } });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
