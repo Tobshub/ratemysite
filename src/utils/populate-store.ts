c6da734d2e95687bbf0e3da567b040f6eb1e1f22
Tobshub <tobanigeorge@gmail.com>
2023-11-20 06:11:11 +0100

    fix: remove token if invalid

--- src/utils/populate-store.ts
+++ src/utils/populate-store.ts
@@ -1,9 +1,13 @@
+"use client";
+
+import { useRouter } from "next/router";
 import { api } from "./api";
 import { ClientToken } from "./client_token";
 import { UserStore } from "./global-store";
 
 export function PopulateUserStore() {
   console.log("attempting to populate user store");
+  const router = useRouter();
   const res = api.profile.getWithPostId.useQuery();
   if (!ClientToken.get()) {
     console.log("populate user store error: no token");
@@ -12,6 +16,12 @@ export function PopulateUserStore() {
   const check = UserStore.get("user");
   if (check.name) {
     console.log("populate user store skip: store not empty");
+    return;
+  }
+  if (res.error?.message === "User profile not found") {
+    console.log("populate user store error: profile not found");
+    ClientToken.remove();
+    router.reload();
     return;
   }
   if (res.data) {
